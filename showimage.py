48898ec852cd7afe354b2ad7358fc7e638300d7f
fsargento629 <sargento228@gmail.com>
2018-12-15 12:20:06 +0000

    oof

--- showimage.py
+++ showimage.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import tkinter as tk
 from PIL import Image, ImageTk  # Place this at the end (to avoid any conflicts/errors)
 
@@ -8,14 +7,3 @@ imagefile = "pics/dankmemes_1.jpg"
 img = ImageTk.PhotoImage(Image.open(imagefile))
 lbl = tk.Label(window, image = img).pack()
 window.mainloop()
-=======
-from PIL import Image
-import io
-from urllib.request import urlopen
-fd = urlopen("https://i.redd.it/w6owupes91421.jpg")
-
-
-image_file = io.BytesIO(fd.read())
-im = Image.open(image_file)
-im.size
->>>>>>> 0cc4da18f6c18658f9d0a10f1402eafeaa929a9e
