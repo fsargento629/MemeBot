1ac676daa1e06fc229a21a7f9737663c629fcc0a
fsargento629 <sargento228@gmail.com>
2018-12-24 18:05:31 +0000

    Cool

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import datetime
 import csv
 import tkinter as tk
 from PIL import Image, ImageTk  # Place this at the end (to avoid any conflicts/errors)
-
+import requests
 class subreddit:
     
     average_grade = 0
@@ -46,8 +46,14 @@ def ScrapeImages(subreddit,date):
         print(image['src'])
         f.write(date + "," + image['src'] + ","+ str(subreddit.meme_count) + "\n")
 
-def DownloadImages(subreddit):
-        pass
+def DownloadImage(subreddit,id,url):
+        
+        #save image
+        img_data = requests.get(url).content
+        with open('pics/'+subreddit.name+"_"+str(id)+".jpeg", 'wb') as handler:
+                handler.write(img_data)
+        with open('pics/'+subreddit.name+".csv","a") as log_file:
+                log_file.write(id+","+ str(datetime.datetime.today()))
 
 
 
@@ -62,4 +68,23 @@ meme_sources = CreateMemeSourcesList("subreddit_list.csv")
 for meme_source in meme_sources:
          print(meme_source.name +" "+ str(meme_source.meme_count))
          print("--------------")
-         ScrapeImages(meme_source,str(datetime.datetime.today()))
+         #ScrapeImages(meme_source,str(datetime.datetime.today()))
+# code to download images:
+
+for meme_source in meme_sources:
+        saved_memes = []
+        known_memes = []
+        
+        with open("pics/"+meme_source.name+".csv","r+") as saved_memes_file:
+                with open("memes/"+meme_source.name+".csv","r+") as known_memes_file:
+                        saved_memes_reader = csv.reader(saved_memes_file)
+                        known_memes_reader = csv.reader(known_memes_file)
+                        for saved_meme in saved_memes_reader:
+                                saved_memes.append(saved_meme[0])
+                        for known_meme in known_memes_reader:
+                                known_memes.append([ known_meme[1],known_meme[2] ] )  #url and id
+                        for meme in known_memes:
+                                if not (meme[1] in saved_memes):
+                                        DownloadImage(meme_source,meme[1],meme[0])
+                                
+
